a841e37c720be9538afc2ac8d724086b0e83ef34
Haroon <haroonkhaleeq@yahoo.com>
2017-01-24 17:09:26 +0100

    DEVELOP: developed stack for the program declaration and functions invocation

--- analysis/analysis_hk1.js
+++ analysis/analysis_hk1.js
@@ -1,24 +1,34 @@
 
 var function_list = [];
+var program_stack = [];
 
 (function (sandbox) {
     function MyAnalysis() {
 
         this.invokeFunPre = function (iid, f, base, args, isConstructor, isMethod, functionIid, functionSid) {
-            console.log(f);
-            console.log(base.global);
-            //console.log(J$.smap[1][121]);
-            //console.log(J$.smap[1][65]); 
+            program_stack.push('invoke-fun-pre_' + f.name);
             function_list.push(functionIid);
             return {f: f, base: base, args: args, skip: false};
         };
 
+        this.invokeFun = function (iid, f, base, args, result, isConstructor, isMethod, functionIid, functionSid) {
+			program_stack.push('invoke-fun_' + f.name);
+			return {result: result};
+        };
+
+		this.declare = function (iid, name, val, isArgument, argumentIndex, isCatchParam) {
+			program_stack.push('declare_' + name);
+			//console.log(iid + ' : ' + name);
+            return {result: val};
+        };
+
         this.scriptExit = function (iid, wrappedExceptionVal) {
             console.log(function_list);
+			console.log(program_stack);
+			console.log(program_stack.pop());
             return {wrappedExceptionVal: wrappedExceptionVal, isBacktrack: false};
         };
 
-  
         this.onReady = function (cb) {
             cb();
         };
