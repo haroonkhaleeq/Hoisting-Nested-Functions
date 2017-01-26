5eaafbd7424b024d2cb456d4a4455fe108b8a3e5
Haroon <haroonkhaleeq@yahoo.com>
2017-01-26 17:39:48 +0100

    DEVELOP: updated anaöysis file for getting all the nested functions of a function

--- analysis/analysis_hk1.js
+++ analysis/analysis_hk1.js
@@ -7,7 +7,7 @@ var program_stack = [];
 
         this.invokeFunPre = function (iid, f, base, args, isConstructor, isMethod, functionIid, functionSid) {
             program_stack.push('invoke-fun-pre_' + f.name);
-            function_list.push(functionIid);
+            function_list.push(f.name);			
             return {f: f, base: base, args: args, skip: false};
         };
 
@@ -25,19 +25,58 @@ var program_stack = [];
         this.scriptExit = function (iid, wrappedExceptionVal) {
             console.log(function_list);
 			console.log(program_stack);
-			console.log(program_stack.pop());
-            return {wrappedExceptionVal: wrappedExceptionVal, isBacktrack: false};
+			check_program_trace();
+			return {wrappedExceptionVal: wrappedExceptionVal, isBacktrack: false};
         };
 
         this.onReady = function (cb) {
             cb();
         };
     }
-
-    sandbox.analysis = new MyAnalysis();
-    console.log('Hello!');
-
+    
+	sandbox.analysis = new MyAnalysis();
 })(J$);
 
+function get_nested_functions(f, i){
+	var match = false;
+	var result = [];
+	while(match == false){
+		i++;
+		//console.log("starting loop with index: " + i);
+				
+//console.log(program_stack[i].includes('declare_'));
+//console.log(function_list.includes(program_stack[i].split('_').splice(-1)[0]));
 
+
+		if(program_stack[i].includes('declare_') && function_list.includes(program_stack[i].split('_').splice(-1)[0])){
+			//console.log(program_stack[i].includes('declare_'));
+			//console.log(function_list.includes(program_stack[i].split('_').splice(-1)[0]));
+			result.push(program_stack[i].split('_').splice(-1)[0]);
+		}
+
+	//program_stack[i].includes('invoke_' + f)
+		
+		//console.log(program_stack[i] === 'invoke-fun_'+f);
+		if(program_stack[i] === 'invoke-fun_'+f){
+			match = true;
+			//console.log('match is true in index: ' + i);
+		}
+
+		//if(i == 8){match = true;}
+	}	
+	
+	//console.log(result);
+	return result;
+}
+
+function check_program_trace(){
+	for(i=0; i<program_stack.length; i++){
+		if(program_stack[i].includes('invoke-fun-pre')){
+			var f = program_stack[i].split('_').splice(-1)[0];
+			var f_nested_functions = get_nested_functions(f, i);
+			console.log(f + ' has these nested functions: ' + f_nested_functions);
+		}
+	}
+
+}
 
