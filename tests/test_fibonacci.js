23de00a5ebf66aa5b8255190c1048fe11f367c55
Pranay Sarkar <pranaysarkar22@gmail.com>
2017-02-15 23:36:03 +0100

    DEVELOP: New test case - recursive function - fibonacci added

---
+++ tests/test_fibonacci.js
@@ -0,0 +1,14 @@
+/*
+* Test case for recursive function
+* Fibonacci Series 
+*/
+function fibonacci(n) {
+	if (n == 0 || n == 1)
+		return n;
+	else
+		return fibonacci(n - 1) + fibonacci(n - 2);
+
+}
+
+fibonacci(2);
+fibonacci(5);
